5967fe5819c2e816294364f86738e8582d47e70c
jose-calderon1996 <joseignaciocalderonroldan39@gmail.com>
2024-11-19 16:09:20 -0300

    can active

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,43 +1,47 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard'; // Importa la guard
 
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./home/home.module').then((m) => m.HomePageModule),
   },
   {
     path: '',
     redirectTo: 'home',
-    pathMatch: 'full'
+    pathMatch: 'full',
   },
   {
     path: 'cambio-pass',
-    loadChildren: () => import('./cambio-pass/cambio-pass.module').then( m => m.CambioPassPageModule)
+    loadChildren: () => import('./cambio-pass/cambio-pass.module').then((m) => m.CambioPassPageModule),
+    canActivate: [AuthGuard], // Protege la ruta
   },
   {
     path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+    loadChildren: () => import('./login/login.module').then((m) => m.LoginPageModule),
   },
   {
     path: 'perfil',
-    loadChildren: () => import('./perfil/perfil.module').then( m => m.PerfilPageModule)
+    loadChildren: () => import('./perfil/perfil.module').then((m) => m.PerfilPageModule),
+    canActivate: [AuthGuard], // Protege la ruta
   },
   {
     path: 'registro-estudiantil',
-    loadChildren: () => import('./registro-estudiantil/registro-estudiantil.module').then( m => m.RegistroEstudiantilPageModule)
+    loadChildren: () =>
+      import('./registro-estudiantil/registro-estudiantil.module').then((m) => m.RegistroEstudiantilPageModule),
   },
   {
     path: 'scanner-qr',
-    loadChildren: () => import('./scanner-qr/scanner-qr.module').then( m => m.ScannerQrPageModule)
-  }
-
+    loadChildren: () => import('./scanner-qr/scanner-qr.module').then((m) => m.ScannerQrPageModule),
+    canActivate: [AuthGuard], // Protege la ruta
+  },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
